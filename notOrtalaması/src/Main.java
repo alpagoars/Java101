f203a05f2a1f845b05e1c129b186a2105f4942d1
Samet Arslan <92922917+alpagoars@users.noreply.github.com>
2023-03-31 01:50:12 +0300

    Update Main.java

--- notOrtalaması/src/Main.java
+++ notOrtalaması/src/Main.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int turkce, matematik, fizik;
+      int matematik, fizik, kimya, turkce, tarih, muzik;
         double ortalama=0;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Türkçe Notunuzu Giriniz:");
-        turkce = scanner.nextInt();
-
         System.out.println("Matematik Notunuzu Giriniz:");
         matematik = scanner.nextInt();
 
-
         System.out.println("Fizik Notunuzu Giriniz:");
-        fizik = scanner.nextInt();
+        fizizk = scanner.nextInt();
 
+        System.out.println("Turkce Notunuzu Giriniz:");
+        turkce = scanner.nextInt();
 
-        ortalama = (matematik + fizik + turkce) /3;
+        System.out.println("Kimya Notunuzu Giriniz:");
+        kimya = scanner.nextInt();
+
+        System.out.println("Tarih Notunuzu Giriniz:");
+        tarih = scanner.nextInt();
+
+        System.out.println("Muzik Notunuzu Giriniz:");
+        muziik = scanner.nextInt();
+
+        ortalama = (mat + fiz + tur + kim + tar + muz)/6;
+
+        ortalama = (matematik + fizik + turkce + kimya + tarih + muzik)/6;
         System.out.println("Ortalama : " + ortalama);
         System.out.println(ortalama>=60?"Sınıfı Geçtiniz":"Sınıfta Kaldınız");
     }
